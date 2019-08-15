4e3c084f0154ff8997d15f6766af8bddc0097cd4
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-08-15 18:54:37 +0200

    pre commit test

--- src/anchoring.spec.ts
+++ src/anchoring.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { expect } from 'chai';
 
-import { Anchoring, AnchorParam } from './anchoring';
+import { Anchoring } from './anchoring';
 import { ApiPromise } from '@polkadot/api';
 import { connect } from './connect';
 import { AccountManager } from './account_manager';
@@ -31,7 +31,7 @@ describe('Anchoring', async () => {
     await accMan.createTestAccounts(api, 3, 1000000);
   });
 
-  it('should commit anchor', (cb) => {
+  it('should commit anchor and not allow the same to be committed again', (cb) => {
     const anchorer = new Anchoring(api);
     let ancParam = newRandomCommitParam();
     anchorer.commit(ancParam)
@@ -59,7 +59,7 @@ describe('Anchoring', async () => {
       });
   });
 
-  it('should pre-commit anchor', (cb) => {
+  it('should pre-commit anchor and not allow to pre-commit the same before expiration', (cb) => {
     const anchorer = new Anchoring(api);
     let ancParam = newRandomAnchorParams();
     anchorer.preCommit(ancParam.preAnchorParam)
@@ -69,7 +69,42 @@ describe('Anchoring', async () => {
           let anchor = await anchorer.findPreAnchor(ancParam.preAnchorParam.anchorId);
           expect(anchor.signingRoot).to.equal(ancParam.preAnchorParam.signingRoot);
           expect(anchor.identity).to.equal(u8aToHex(accMan.getAccountByIndex(0).publicKey));
-          cb();
+
+          // pre-committing same anchor before expiration of previous pre-commit must FAIL
+          anchorer.preCommit(ancParam.preAnchorParam)
+            .signAndSend(accMan.getAccountByIndex(1), async ({ events = [], status }) => {
+              //console.log('Transaction status:', status.type);
+              if (status.isFinalized) {
+                events.forEach(async ({ phase, event: { data, method, section } }) => {
+                  if (section === 'system') {
+                    expect(method).to.equal('ExtrinsicFailed');
+                    cb();
+                  }
+                });
+              }
+            });
+        }
+      });
+  });
+
+  it('should pre-commit and the commit anchor with document proof', (cb) => {
+    const anchorer = new Anchoring(api);
+    let ancParam = newRandomAnchorParams();
+    anchorer.preCommit(ancParam.preAnchorParam)
+      .signAndSend(accMan.getAccountByIndex(0), async ({ events = [], status }) => {
+        //console.log('Transaction status:', status.type);
+        if (status.isFinalized) {
+          // pre-committing same anchor before expiration of previous pre-commit must FAIL
+          anchorer.commit(ancParam.anchorParam)
+            .signAndSend(accMan.getAccountByIndex(0), async ({ events = [], status }) => {
+              //console.log('Transaction status:', status.type);
+              if (status.isFinalized) {
+                let anchor = await anchorer.findAnchor(ancParam.anchorParam.getAnchorId());
+                expect(anchor.docRoot).to.equal(ancParam.anchorParam.docRoot);
+                expect(anchor.id).to.equal(ancParam.anchorParam.getAnchorId());
+                cb();
+              }
+            });
         }
       });
   });
