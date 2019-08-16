b24583d018f444ef4dc01048b0db365be872c801
Vimukthi <vimukthi@centrifuge.io>
2019-08-16 13:13:33 +0200

    Read configs from env or file (#5)
    * Read configs from env or file
    
    * Read configs from env or file
    
    * Global test state
    
    * Global test state

--- src/anchoring.spec.ts
+++ src/anchoring.spec.ts
@@ -2,43 +2,19 @@ import 'mocha';
 import { expect } from 'chai';
 
 import { Anchoring } from './anchoring';
-import { ApiPromise } from '@polkadot/api';
-import { connect } from './connect';
-import { AccountManager } from './account_manager';
-
-// TODO read from env
-const WS_PROVIDER='ws://127.0.0.1:9944';
+import { TestGlobals } from './test_globals';
 
 describe('Anchoring', async () => {
-
-  let api: ApiPromise;
-
-  before(async () => {
-    api = await connect(WS_PROVIDER);
-    const [chain, nodeName, nodeVersion] = await Promise.all([
-      api.rpc.system.chain(),
-      api.rpc.system.name(),
-      api.rpc.system.version()
-    ]);
-    console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
-
-    // TODO move this to a place at the route of all tests later
-    let accMan = new AccountManager();
-    await accMan.createTestAccounts(api, 3, 1000000);
-  });
 
   describe('Commit', async () => {
 
     it('should commit anchor', async () => {
+      console.log(TestGlobals.testConfig);
       const result = new Anchoring();
       //result.commit();
       expect(result).to.equal('Hello world!');
     });
 
-  });
-
-  after(async () => {
-    api.disconnect();
   });
 
 });
