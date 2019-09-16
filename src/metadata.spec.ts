1095a5bfe61686abafee39f8ca990ed1475d2f16
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-09-16 16:06:56 +0200

    minor

--- src/metadata.spec.ts
+++ src/metadata.spec.ts
@@ -5,7 +5,7 @@ import { TestGlobals } from './test_globals';
 
 describe('Metadata', () => {
 
-  it.only('should list out all public extrinsics in the system', async () => {
+  it('should list out all public extrinsics in the system', async () => {
       let extrinsics = [];
       let count = 0;
       Object.keys(TestGlobals.connection.api.tx).forEach(key => {
