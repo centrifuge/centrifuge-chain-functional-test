8c3ca1df3fd355d906170d2b08f7e660a16959a9
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-01-14 23:02:07 +0100

    Add commits to tests as well, simplify load test

--- src/load_test.spec.ts
+++ src/load_test.spec.ts
@@ -1,10 +1,7 @@
-import { expect } from "chai";
-import "mocha";
-
+import { SubmittableExtrinsic } from "@polkadot/api/promise/types";
 import { KeyringPair } from "@polkadot/keyring/types";
 import BN = require("bn.js");
 import { Anchoring } from "./anchoring";
-import { senderFunction } from "./balance";
 import { TestGlobals } from "./test_globals";
 import { newRandomAnchorParams } from "./testutil";
 
@@ -13,163 +10,142 @@ interface ISender {
   nonce: BN;
 }
 
-describe.only("Load tests", () => {
+describe("Load tests", () => {
   it("should send many txs in parallel and print aggregated statistics", async () => {
     const sender: ISender = { keypair: TestGlobals.accMan.getAccountByIndex(0), nonce: new BN(0) };
     sender.nonce = await TestGlobals.connection.api.query.system.accountNonce(sender.keypair.address) as any;
 
-    const transfs = await sendBalanceTransfs(sender, 0);
-    const preComms = await sendAnchorPreCommits(sender, 500);
+    const subs: SubmittableExtrinsic[] = [];
 
-    while (true) {
-      const transfsStopped = transfs.filter((t) => t.stopAt !== null);
-      const preCommsStopped = preComms.filter((t) => t.stopAt !== null);
+    // make sure not to use more than a total of 512 xts, ready pool has a limit of 512.
+    addBalanceTransfs(subs, 170);
+    addAchorPrecomms(subs, 170);
+    addAchorComms(subs, 170);
 
-      console.log(`balance_transfers: ` +
-        `pending: ${transfs.filter((t) => t.status === "pending").length} | ` +
-        `future: ${transfs.filter((t) => t.status === "future").length} | ` +
-        `ready: ${transfs.filter((t) => t.status === "ready").length} | ` +
-        `finalized: ${transfs.filter((t) => t.status === "finalized").length} | ` +
-        `usurped: ${transfs.filter((t) => t.status === "usurped").length} | ` +
-        `broadcast: ${transfs.filter((t) => t.status === "broadcast").length} | ` +
-        `dropped: ${transfs.filter((t) => t.status === "dropped").length} | ` +
-        `invalid: ${transfs.filter((t) => t.status === "invalid").length} || ` +
-        `avgMs: ${transfsStopped.reduce((sum, t) => sum + t.stopAt! - t.startAt, 0) / transfsStopped.length}`);
+    shuffle(subs);
 
-      console.log(`anchor_preCommits: ` +
-        `pending: ${preComms.filter((t) => t.status === "pending").length} | ` +
-        `future: ${preComms.filter((t) => t.status === "future").length} | ` +
-        `ready: ${preComms.filter((t) => t.status === "ready").length} | ` +
-        `finalized: ${preComms.filter((t) => t.status === "finalized").length} | ` +
-        `usurped: ${preComms.filter((t) => t.status === "usurped").length} | ` +
-        `broadcast: ${preComms.filter((t) => t.status === "broadcast").length} | ` +
-        `dropped: ${preComms.filter((t) => t.status === "dropped").length} | ` +
-        `invalid: ${preComms.filter((t) => t.status === "invalid").length} || ` +
-        `avgMs: ${preCommsStopped.reduce((sum, t) => sum + t.stopAt! - t.startAt, 0) / preCommsStopped.length}`);
+    const exts = await signAndSend(sender, subs);
+
+    for (let i = 0; i < 30; i++) {
+      const stopped = exts.filter((t) => t.stopAt !== null);
+      const finalized = exts.filter((t) => t.status === "finalized");
+
+      console.log(`extrinsics: ` +
+        `pending: ${exts.filter((t) => t.status === "pending").length} | ` +
+        `future: ${exts.filter((t) => t.status === "future").length} | ` +
+        `ready: ${exts.filter((t) => t.status === "ready").length} | ` +
+        `finalized: ${finalized.length} | ` +
+        `usurped: ${exts.filter((t) => t.status === "usurped").length} | ` +
+        `broadcast: ${exts.filter((t) => t.status === "broadcast").length} | ` +
+        `dropped: ${exts.filter((t) => t.status === "dropped").length} | ` +
+        `invalid: ${exts.filter((t) => t.status === "invalid").length} || ` +
+        `avgMs: ${stopped.length && stopped.reduce((sum, t) => sum + t.stopAt! - t.startAt, 0) / stopped.length}`);
+
+      if (finalized.length === exts.length) {
+        return;
+      }
 
       await sleep(1000);
     }
+
+    throw new Error("did not finalize all extrinsics in given time");
   });
 });
 
 interface IExt {
-  i: number;
   startAt: number;
-  stopAt: number|null;
-  status: "pending"|"future"|"ready"|"finalized"|"usurped"|"broadcast"|"dropped"|"invalid";
+  stopAt: number | null;
+  status: "pending" | "future" | "ready" | "finalized" | "usurped" | "broadcast" | "dropped" | "invalid";
 }
 
-async function sendBalanceTransfs(sender: ISender, n: number): Promise<IExt[]> {
-  const transfers: IExt[] = [];
-  // const senderBalance = await TestGlobals.connection.api.query.balances.freeBalance(sender.keypair.address);
-  // console.log("Balance for account " + sender.keypair.address + ": " + senderBalance);
+function addBalanceTransfs(subs: SubmittableExtrinsic[], n: number) {
   const charlie = TestGlobals.accMan.getAccountByIndex(2);
 
   for (let i = 0; i < n; i++) {
-    const transfer: IExt = {
-      i,
-      startAt: Date.now(),
-      status: "pending",
-      stopAt: null,
-    };
-
-    TestGlobals.connection.api.tx.balances.transfer(charlie.address, 1000).sign(sender.keypair, { nonce: sender.nonce })
-      .send(({ events = [], status }) => {
-        // console.log("Transaction status:", status.type);
-
-        if (status.isFuture) {
-          transfer.status = "future";
-        } else if (status.isReady) {
-          transfer.status = "ready";
-        } else if (status.isFinalized) {
-          transfer.status = "finalized";
-
-          transfer.stopAt = Date.now();
-          // console.log("Completed at block hash", status.asFinalized.toHex());
-          // console.log("Events:");
-          // events.forEach(({ phase, event: { data, method, section } }) => {
-          //   console.log("\t", phase.toString(), `: ${section}.${method}`, data.toString());
-          // });
-        } else if (status.isUsurped) {
-          transfer.status = "usurped";
-        } else if (status.isBroadcast) {
-          transfer.status = "broadcast";
-        } else if (status.isDropped) {
-          transfer.status = "dropped";
-          transfer.stopAt = Date.now();
-        } else if (status.isInvalid) {
-            transfer.status = "invalid";
-            transfer.stopAt = Date.now();
-        } else {
-          throw new Error(`unexpected status ${status}`);
-        }
-    });
-
-    transfers.push(transfer);
-
-    // update nonce
-    sender.nonce.iaddn(1);
+    subs.push(TestGlobals.connection.api.tx.balances.transfer(charlie.address, 1000));
   }
-
-  return transfers;
 }
 
-async function sendAnchorPreCommits(sender: ISender, n: number): Promise<IExt[]> {
-  const anchors: IExt[] = [];
-
+function addAchorPrecomms(subs: SubmittableExtrinsic[], n: number) {
   const anchorer = new Anchoring(TestGlobals.connection);
 
   for (let i = 0; i < n; i++) {
     const ancParam = newRandomAnchorParams();
+    subs.push(anchorer.preCommit(ancParam.preCommitParam));
+  }
+}
 
-    const anchor: IExt = {
-      i,
+function addAchorComms(subs: SubmittableExtrinsic[], n: number) {
+  const anchorer = new Anchoring(TestGlobals.connection);
+
+  for (let i = 0; i < n; i++) {
+    const ancParam = newRandomAnchorParams();
+    subs.push(anchorer.commit(ancParam.anchorParam));
+  }
+}
+
+async function signAndSend(sender: ISender, subs: SubmittableExtrinsic[]) {
+  const exts: IExt[] = [];
+  // const senderBalance = await TestGlobals.connection.api.query.balances.freeBalance(sender.keypair.address);
+  // console.log("Balance for account " + sender.keypair.address + ": " + senderBalance);
+
+  for (const sub of subs) {
+
+    const ext: IExt = {
       startAt: Date.now(),
       status: "pending",
       stopAt: null,
     };
 
-    anchorer.preCommit(ancParam.preCommitParam)
-      .signAndSend(sender.keypair, { nonce: sender.nonce }, async ({ events = [], status }) => {
+    sub.sign(sender.keypair, { nonce: sender.nonce })
+      .send(({ events = [], status }) => {
         // console.log("Transaction status:", status.type);
 
         if (status.isFuture) {
-          anchor.status = "future";
+          ext.status = "future";
         } else if (status.isReady) {
-          anchor.status = "ready";
+          ext.status = "ready";
         } else if (status.isFinalized) {
-          anchor.status = "finalized";
+          ext.status = "finalized";
 
-          anchor.stopAt = Date.now();
+          ext.stopAt = Date.now();
           // console.log("Completed at block hash", status.asFinalized.toHex());
           // console.log("Events:");
           // events.forEach(({ phase, event: { data, method, section } }) => {
           //   console.log("\t", phase.toString(), `: ${section}.${method}`, data.toString());
           // });
         } else if (status.isUsurped) {
-          anchor.status = "usurped";
+          ext.status = "usurped";
         } else if (status.isBroadcast) {
-          anchor.status = "broadcast";
+          ext.status = "broadcast";
         } else if (status.isDropped) {
-          anchor.status = "dropped";
-          anchor.stopAt = Date.now();
+          ext.status = "dropped";
+          ext.stopAt = Date.now();
         } else if (status.isInvalid) {
-            anchor.status = "invalid";
-            anchor.stopAt = Date.now();
+          ext.status = "invalid";
+          ext.stopAt = Date.now();
         } else {
           throw new Error(`unexpected status ${status}`);
         }
       });
 
-    anchors.push(anchor);
+    exts.push(ext);
 
     // update nonce
     sender.nonce.iaddn(1);
   }
 
-  return anchors;
+  return exts;
 }
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function shuffle<T>(a: T[]) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
