7562ccfa4a7df79b4a422e9ebd3e69daa0dcda1b
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-01-14 16:53:20 +0100

    Add metadata count test (previously #15)

---
+++ src/metadata.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import "mocha";
+
+import { TestGlobals } from "./test_globals";
+
+describe("Metadata", () => {
+  it("should list out all public calls in the system", async () => {
+      const extrinsics = [];
+      let count = 0;
+      Object.keys(TestGlobals.connection.api.tx).forEach((mod) => {
+        Object.keys(TestGlobals.connection.api.tx[mod]).forEach((call) => {
+            extrinsics.push(mod + "." + call);
+            // printing for documentation
+            console.log("|" + count + "|", mod, "|", call, "|-|");
+            count++;
+        });
+      });
+      expect(count).to.equal(47);
+      // dummy check
+      expect(extrinsics.length).to.equal(count);
+  });
+});
